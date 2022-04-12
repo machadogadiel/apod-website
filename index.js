7a1de4b997af787fba95f37c2080444bf0bcdcf5
machadogadiel <github@gadiel.dev>
2022-04-12 17:13:41 -0300

    created project base

---
+++ index.js
@@ -0,0 +1,11 @@
+let request = new XMLHttpRequest()
+
+request.open("GET", "https://api.nasa.gov/planetary/apod?api_key=0rVXRVo6o5fdEbBwflG2ogibzzDXNcT5gNngfVF2")
+
+request.onload = function() {
+    if (requisicao.status == 200) {
+        
+    } else {
+        
+    }
+}
